c035775f5e911cb8397c98d99a41b7cfc261c874
s-kganz <ganzkeenan1@gmail.com>
2019-05-14 16:15:11 -0400

    bec.py now adds orders to database

--- bec.py
+++ bec.py
@@ -1,14 +1,30 @@
 from flask import Flask, request, session, flash, redirect, render_template
 from forms import OrderForm
 
+from secrets.config import Config, TestingConfig, DebugConfig
+
 app = Flask(__name__)
 app.secret_key = "Who knows?"
+app.config.from_object(TestingConfig)
+
+# Conditional import for working with dummy database
+if app.config["DEBUG"]:
+    from db_helper import MockHelper as DBHelper
+else:
+    from db_helper import DBHelper
+
+DB = DBHelper()
 
 @app.route('/order', methods=["POST", "GET"])
 def getOrder():
     form = OrderForm()
     if form.validate_on_submit():
-        #TODO add order to database
+        DB.add_order(form.name.data,
+                     form.location.data,
+                     form.bec.data,
+                     form.ec.data,
+                     form.be.data,
+                     form.comments.data)
         flash("Thank you for your order!")
         return redirect('/')
     return render_template("order.html", form=form)
