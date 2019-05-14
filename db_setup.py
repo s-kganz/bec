2dba94dcaa702f2348c6f358a18e5c4411f401e5
s-kganz <ganzkeenan1@gmail.com>
2019-05-14 15:38:58 -0400

    added mysql database setup script

---
+++ db_setup.py
@@ -0,0 +1,24 @@
+import pymysql
+from secrets.dbconfig import password, username
+
+connection = pymysql.connect(host="localhost",
+                             user=username,
+                             password=password)
+
+try:
+    with connection.cursor() as cursor:
+        sql = "CREATE DATABASE IF NOT EXISTS bec"
+        cursor.execute(sql)
+        sql = """CREATE TABLE IF NOT EXISTS bec.orders(id int NOT NULL AUTO_INCREMENT,
+                                                       name VARCHAR(50) NOT NULL,
+                                                       location VARCHAR(50) NOT NULL,
+                                                       bec_count int UNSIGNED NOT NULL,
+                                                       ec_count int UNSIGNED NOT NULL,
+                                                       be_count int UNSIGNED NOT NULL,
+                                                       comment VARCHAR(1000),
+                                                       PRIMARY KEY (id))
+             """
+        cursor.execute(sql)
+    connection.commit()
+finally:
+    connection.close()
