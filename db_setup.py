b355cf1220cc48a87c89c0eec33ca91d166e5bd8
s-kganz <ganzkeenan1@gmail.com>
2019-05-14 16:58:05 -0400

    add user table to database

--- db_setup.py
+++ db_setup.py
@@ -7,10 +7,12 @@ connection = pymysql.connect(host="localhost",
 
 try:
     with connection.cursor() as cursor:
+        # Create database
         sql = "DROP DATABASE IF EXISTS bec"
         cursor.execute(sql)
         sql = "CREATE DATABASE IF NOT EXISTS bec"
         cursor.execute(sql)
+        # Create order table
         sql = """CREATE TABLE IF NOT EXISTS bec.orders(id int NOT NULL AUTO_INCREMENT,
                                                        name VARCHAR(50) NOT NULL,
                                                        location VARCHAR(50) NOT NULL,
@@ -22,6 +24,13 @@ try:
                                                        ts TIMESTAMP NOT NULL,
                                                        PRIMARY KEY (id))
              """
+        cursor.execute(sql)
+        # Create user table
+        sql = """CREATE TABLE IF NOT EXISTS bec.users(user VARCHAR(12) NOT NULL,
+                                                      passhash VARCHAR(100) NOT NULL,
+                                                      ts TIMESTAMP NOT NULL,
+                                                      PRIMARY KEY (user))
+              """
         cursor.execute(sql)
     connection.commit()
 finally:
